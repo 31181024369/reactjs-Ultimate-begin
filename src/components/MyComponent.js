302d383c7098e8410e36e5c571208b7cb14f963d
longhoang <long542.nt@gmail.com>
2025-02-26 23:05:57 +0700

    chuong2-bai19-26-2-2025

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,18 @@
 import React from "react";
 class MyComponent extends React.Component{
+    state={
+        name:'haryPhamDev',
+        address:'hoidanit',
+        age:26
+    }
+    handleClick(event){
+        console.log("function click");
+    };
     render(){
         return (
             <div>
-                my first component {Math.random()}
+               my name is {this.state.name} and age :{this.state.age}
+               <button onClick={this.handleClick}>click</button>
             </div>
         )
     }

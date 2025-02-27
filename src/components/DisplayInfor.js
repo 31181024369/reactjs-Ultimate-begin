5ed32f18984a0f65cea6bb3c39f7f3ca135269c6
longhoang <long542.nt@gmail.com>
2025-02-27 23:09:34 +0700

    chuong2-bai23-27-2-2025

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,13 @@
+import React from "react";
+class DisplayInfor extends React.Component{
+    render(){
+        const {name,age}=this.props;
+        return (
+            <div>
+                <div>My name is {name}</div>
+                <div>My age's {age}</div>
+            </div>
+        );
+    }
+}
+export default DisplayInfor
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,15 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component{
     render(){
         return (
             <div>
               <UserInfor></UserInfor>
+              <br></br>
+              <DisplayInfor name="hoanglong" age="23"></DisplayInfor>
+              <hr></hr>
+             
             </div>
         )
     }

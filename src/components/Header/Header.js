2900f0c63c089ee2ea01dd297bab23720d13023c
longhoang <long542.nt@gmail.com>
2025-03-03 21:27:00 +0700

    chuong5-bai53-3-3-2025

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,13 +19,15 @@ const Header=()=>{
             <Nav.Link href="#link">Admin</Nav.Link> */}
           </Nav>
           <Nav>
-          <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
+          {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="">Log in</NavDropdown.Item>
               <NavDropdown.Item href="">
                 Log out
               </NavDropdown.Item>
               <NavDropdown.Item href="">Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -2,10 +2,19 @@ import videoHomepage from '../../assets/video-homepage.mp4';
 const HomePage=(props)=>{
     return (
         <div className="homepage-container">
-        <video autoPlay muted loop>
-                <source src={videoHomepage} type ="video/mp4"/>
-        </video>
+            <video autoPlay muted loop>
+                    <source src={videoHomepage} type ="video/mp4"/>
+            </video>
+            <div className='homepage-content'>
+                <div className='title-1'>There's a better way a ask</div>
+                <div className='title-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
+                <div className='title-3'>
+                    <button>Get's start.It's free</button>
+                </div>
+            </div>
         </div>
+        
     );
 }
 export default HomePage;

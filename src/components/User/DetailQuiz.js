144dd75c46b271eca6aba1d2579a3e70d3c327ad
longhoang <long542.nt@gmail.com>
2025-03-16 11:10:31 +0700

    chuong8-bai86-16-3-2025

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -46,6 +46,25 @@ const DetailQuiz=(props)=>{
             setIndex(index+1);
         }
     }
+    const handleCheckoutbox=(answerId,questionId)=>{
+        let dataQuizClone=_.cloneDeep(dataQuiz);
+        let question=dataQuizClone.find(item=>+item.questionId===+questionId)
+        if(question && question.answers){
+            question.answers=question.answers.map(item=>{
+                if(+item.id===+answerId){
+                    item.isSelected=!item.isSelected;
+                }
+                return item;
+            })
+            // question.answers=b;
+        }
+        let index=dataQuizClone.findIndex(item=>+item.questionId===+questionId);
+        if(index>-1){
+            dataQuizClone[index]=question;
+            setDataQuiz(dataQuizClone);
+        }
+        console.log("data:",dataQuizClone);
+    }
     return (
         <div className="detail-quiz-container">
             <div className="left-content">
@@ -55,11 +74,12 @@ const DetailQuiz=(props)=>{
                 <div className="q-body">
                 </div>
                 <div className="q-content">
-                    <Question index={index} data={dataQuiz && dataQuiz.length>0 ?dataQuiz[index]:[]}></Question>
+                    <Question handleCheckoutbox={handleCheckoutbox}  index={index} data={dataQuiz && dataQuiz.length>0 ?dataQuiz[index]:[]}></Question>
                 </div>
                 <div className="footer">
                     <button onClick={()=>handlePrev()} className="btn btn-primary">Prev</button>
                     <button onClick={()=>handleNext()} className="btn btn-secondary">Next</button>
+                    <button onClick={()=>handleNext()} className="btn btn-warning">Finish</button>
                 </div>
             </div>
             <div className="right-content">
--- src/components/User/Question.js
+++ src/components/User/Question.js
@@ -1,27 +1,35 @@
 import _ from 'lodash';
 const Question=(props)=>{
-    const {data,index}=props;
+    const {data,index,handleCheckoutbox}=props;
     if(_.isEmpty(data)){
         return (
             <></>
         );
     }
+    const handleCheckBox=(event,aId,qId)=>{
+        //console.log("check:",event.target.checked);
+          console.log("prod:",aId,qId);
+        handleCheckoutbox(aId,qId);
+
+    }
     return (
         <>
-        {data.image &&
+        {data.image ?
             <div className='q-image'>
                 <img src={`data:image/png;base64,${data.image}`}/>
             </div>
+            :
+            <div className='q-image'>
+            </div>
          }
-           
              <div className="question">Question {index+1}:{data.questionDescription}?</div>
                 <div className="answer">
                     {data.answers && data.answers.length && data.answers.map((a,index)=>{
                         return (
                             <div className="a-child" key={`answer-${index}`}>
-                            <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                            <label class="form-check-label" for="flexCheckDefault">
+                            <div className="form-check">
+                            <input onChange={(event)=>handleCheckBox(event,a.id,data.questionId)} className="form-check-input" type="checkbox"checked={a.isSelected} value="" id="flexCheckDefault" />
+                            <label className="form-check-label" for="flexCheckDefault">
                                 {a.description}
                             </label>
                             </div>
